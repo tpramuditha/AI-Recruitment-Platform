0e143161cc1d1284e59961263fe398baba0d0c48
Thathsara Pramuditha <pramudithamunasinghe11@gmail.com>
2026-06-28 21:30:46 +0530

    Fix JSON circular reference in Applications/Jobs responses

--- RecruitmentPlatform.API/RecruitmentPlatform.API/Program.cs
+++ RecruitmentPlatform.API/RecruitmentPlatform.API/Program.cs
@@ -9,7 +9,11 @@ using Microsoft.OpenApi;
 var builder = WebApplication.CreateBuilder(args);
 
 // 1. Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
+
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(options =>
